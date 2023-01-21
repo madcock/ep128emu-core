40c48bda55e7ee356ae886173754f065ebfae9ce
zoltanvb <101990835+zoltanvb@users.noreply.github.com>
2023-01-21 23:07:20 +0100

    Version bump to 1.2.6

--- core/main.cpp
+++ core/main.cpp
@@ -622,7 +622,7 @@ void retro_get_system_info(struct retro_system_info *info)
 {
   memset(info, 0, sizeof(*info));
   info->library_name     = "ep128emu";
-  info->library_version  = "v1.2.5";
+  info->library_version  = "v1.2.6";
   info->need_fullpath    = true;
   info->valid_extensions = "img|dsk|tap|dtf|com|trn|128|bas|cas|cdt|tzx|wav|tvcwav|.";
 }
